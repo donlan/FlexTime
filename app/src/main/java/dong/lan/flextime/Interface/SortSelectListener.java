c1d445a86547bba02878424cf0682c0b9da82efd
dooze <760625325@qq.com>
2016-05-01 01:29:39 +0800

    排序算法的选择回调接口

---
+++ app/src/main/java/dong/lan/flextime/Interface/SortSelectListener.java
@@ -0,0 +1,12 @@
+package dong.lan.flextime.Interface;
+
+import dong.lan.flextime.bean.Sort;
+
+/**
+ * 项目：FlexTime
+ * 作者：梁桂栋
+ * 日期： 4/29/2016  13:04.
+ */
+public interface SortSelectListener {
+    void onSortSelect(Sort sort);
+}
